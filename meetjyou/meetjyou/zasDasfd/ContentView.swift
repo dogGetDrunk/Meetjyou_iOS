5918fb1b8c5e7a0206d37bb4cd0c183f23d50e61
Jehyeon Lee <dasom8899981@gmail.com>
2025-01-25 14:31:24 +0900

    .

---
+++ meetjyou/meetjyou/zasDasfd/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  meetjyou
+//
+//  Created by LJh on 1/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+// 해야할거 API키
---
+++ meetjyou/meetjyouApp.swift
@@ -0,0 +1,17 @@
+//
+//  meetjyouApp.swift
+//  meetjyou
+//
+//  Created by LJh on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct meetjyouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
