ad87f2a730e2a20c8488b45cd7407324ee2bce5d
Nnn <jaehyung1299@naver.com>
2025-03-18 10:21:13 +0900

    프로필 수정 뷰파일 생성

---
+++ meetjyou/meetjyou/View/Mypage/ProfileEditView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditView.swift
+//  meetjyou
+//
+//  Created by 박재형 on 3/18/25.
+//
+
+import SwiftUI
+
+struct ProfileEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditView()
+}
